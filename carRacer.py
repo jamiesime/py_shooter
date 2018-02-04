1bc1019ba44181e5f758f33bc4405f83f437ec2e
GHOST-HUB\Jamie <jamiesime@gmail.com>
2018-02-04 09:53:14 +0000

    car moves and rotates according to user input

--- carRacer.py
+++ carRacer.py
@@ -32,3 +32,23 @@ class CarSprite(pygame.sprite.Sprite):
 		self.image = pygame.transform.rotate(self.src_image, self.direction)
 		self.rect = self.image.get_rect()
 		self.rect.center = self.position
+
+
+rect = screen.get_rect()
+car = CarSprite('car.png', rect.center)
+car_group = pygame.sprite.RenderPlain(car)
+while 1:
+	deltat = clock.tick(60)
+	for event in pygame.event.get():
+		if not hasattr(event, 'key'): continue
+		down = event.type == KEYDOWN
+		if event.key == K_RIGHT: car.k_right = down * -5
+		elif event.key == K_LEFT: car.k_left = down * 5
+		elif event.key == K_UP: car.k_up = down * 2
+		elif event.key == K_DOWN: car.k_down = down * -2
+		elif event.key == K_ESCAPE: sys.exit(0)
+
+	screen.fill((0,0,0))
+	car_group.update(deltat)
+	car_group.draw(screen)
+	pygame.display.flip()
